9a1af781276ab219a71f23a9d0c905bee3a88073
Ryan Luttrull <reluttrull@gmail.com>
2026-02-04 08:48:36 -0600

    Undo last punch button, show timestamp feedback

--- MyTime.Shifts.Contracts/Responses/PunchResponse.cs
+++ MyTime.Shifts.Contracts/Responses/PunchResponse.cs
@@ -2,5 +2,5 @@
 
 namespace MyTime.Shifts.Contracts.Responses
 {
-    public record PunchResponse(int PunchId, int UserId, DateTime RecordedTime, Enums.PunchType PunchType, bool? IsApproved, int? ApprovedByUser);
+    public record PunchResponse(int Id, int UserId, DateTime RecordedTime, Enums.PunchType PunchType, bool? IsApproved, int? ApprovedByUser);
 }
